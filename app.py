551da1da52912724a1e616108766f10b3c0a5a6e
ATHIDHYA <jathidhya@example.com>
2026-03-18 09:45:47 +0530

    final fix for render deployment

--- app.py
+++ app.py
@@ -346,5 +346,6 @@ def add_user():
 
 # -------------------- RUN --------------------
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
